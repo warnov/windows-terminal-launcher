d4863e04c5650b4f3e89931c8c29a324675cf251
warnov <warnov@gmail.com>
2024-08-18 00:19:42 -0500

    First Commit

---
+++ WTLauncher.ConaoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+
+//Open windows terminal on a separate process in c:\src
+Process.Start("wt.exe", "-d c:\\src");
+
+//Exits the console app freeing all the resources
+Environment.Exit(0);
